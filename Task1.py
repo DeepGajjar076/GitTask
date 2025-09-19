dd955c55f37350cec5857cc5239bd5f5f1339a1b
DeepGajjar076 <deepgajjar076@gmail.com>
2025-09-19 10:46:02 +0530

    initail Commit

---
+++ Task1.py
@@ -0,0 +1,15 @@
+#Take input from the user for two numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+#Perform the mathematical operations
+addition = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+
+#Display the results
+print("Addition:" ,addition)
+print("Subtraction:" ,subtraction)
+print("Multiplication:" ,multiplication)
+print("Division:" ,division)
---
+++ Task2.py
@@ -0,0 +1,9 @@
+#Ask the user for their first and last names
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+#Concatenate the names into a full name
+full_name = first_name + " " + last_name
+
+#Print a personalized greeting
+print("\nHello,",full_name,"! Welcome to the Python program.")
